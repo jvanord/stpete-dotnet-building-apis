a72430326acf6f038f28fd3e37dceb98a5b7408f
Jay VanOrd <jvanord@indasysllc.com>
2017-11-01 10:24:36 -0400

    JSON output

--- ApiDemo/wwwroot/js/test.js
+++ ApiDemo/wwwroot/js/test.js
@@ -10,32 +10,41 @@ var ApiTest = (function (defaultOptions) {
 	me.init = function (overrideOptions) {
 		options = $.extend({}, defaultOptions, overrideOptions);
 		$output = $(options.outputElement);
-		me.log.info('Tests Initialized');
+		me.log.info('Tests Initialized', options);
 	};
 
 	// Logging
 	me.log = (function(){
 		function sendOutput(message, cssClass, tag) {
+			if (!message) return;
 			tag = tag || 'p';
 			$output.append($('<' + tag + '/>').addClass(cssClass).html(message));
 		}
+		function outputObject(obj) {
+			$output.append($('<div/>').addClass('code').text(JSON.stringify(obj, null, '  ')));
+		}
 		return {
-			info: function (message) {
-				console.info(message);
+			info: function (message, obj) {
+				console.info(message, obj);
 				sendOutput(message, 'info');
+				if (!!obj) outputObject(obj);
 			},
-			success: function (message) {
-				console.log(message);
+			success: function (message, obj) {
+				console.log(message, obj);
 				sendOutput(message, 'success');
+				if (!!obj) outputObject(obj);
 			},
-			warning: function (message) {
-				console.warn(message);
+			warning: function (message, obj) {
+				console.warn(message, obj);
 				sendOutput(message, 'warning');
+				if (!!obj) outputObject(obj);
 			},
-			error: function (message) {
-				console.error(message);
+			error: function (message, obj) {
+				console.error(message, obj);
 				sendOutput(message, 'error');
+				if (!!obj) outputObject(obj);
 			},
+			clear: function () { $output.html(''); }
 		};
 	})();
 
